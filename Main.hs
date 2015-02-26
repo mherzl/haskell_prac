1e95d1e66e3406252092a2ee1185a1fa910bc5aa
Matthew Herzl <mherzl@localhost.localdomain>
2015-02-25 20:54:32 -0700

    made a simple haskell task for beaven

---
+++ Main.hs
@@ -0,0 +1,17 @@
+-- Main.hs
+{-
+ - You can compile this program by entering 'ghc Main.hs' in the command line
+ - and you can run the resulting executable 'Main' by entering './Main'
+ -
+ - Do that, then ...
+ -
+ -
+ - Your mission, should you choose to accept it,
+ - is to make this program print a hilarious, hilarious joke
+ -
+ - instead of just the word 'here'.
+ -
+ - may the force be with you
+ -
+-}
+main = putStrLn "here"
